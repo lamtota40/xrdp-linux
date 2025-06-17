6ac53e8fc45e0223b671accb198d853fb076d718
lamtota40 <lamtota40@gmail.com>
2025-06-17 19:04:40 +0700

    Update xrdp.sh

--- xrdp.sh
+++ xrdp.sh
@@ -1,3 +1,7 @@
 sudo chown xrdp:xrdp /etc/xrdp/key.pem
 sudo chmod 600 /etc/xrdp/key.pem
 sudo chown xrdp:xrdp /var/run/xrdp
+sudo usermod -a -G ssl-cert xrdp
+sudo systemctl daemon-reexec
+sudo systemctl daemon-reload
+sudo systemctl restart xrdp xrdp-sesman
